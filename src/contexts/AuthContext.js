353a55c50b70239de25462f5ce3162f0f5353167
Chayan Das <01chayandas@gmail.com>
2025-07-15 14:28:51 +0530

    feat[refresh]: add funtionalty for refresh token for password

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: © 2025 Siemens AG
 // SPDX-FileContributor: Sourav Bhowmik <sourav.bhowmik@siemens.com>
 // SPDX-FileContributor: Dearsh Oberoi <dearsh.oberoi@siemens.com>
+// SPDX-FileContributor: 2025 Chayan Das <01chayandas@gmail.com>
 
 import React, { useContext, useEffect } from 'react';
 import axios from 'axios';
@@ -19,23 +20,30 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
 	useEffect(() => {
-		if (process.env.REACT_APP_PROVIDER === 'oidc') {
-			const interval = setInterval(
-				getAccessTokenFromRefreshToken,
-				30 * 60 * MILLISEC,
-			); // every 30 minutes
-
-			return () => clearInterval(interval);
-		}
+		const interval = setInterval(() => {
+			const refresh_token = localStorage.getItem('licensedb.refresh_token');
+			if (refresh_token) {
+				getAccessTokenFromRefreshToken();
+				console.log('Using refresh token:', refresh_token);
+			}
+		},  30 * 60 * MILLISEC ); // every 30 minutes
+	
+		return () => clearInterval(interval);
 	}, []);
+	
 
 	async function Signin(userCredentialsPayload) {
 		try {
 			const url = `${process.env.REACT_APP_BASE_URL}/login`;
-			const response = await axios.post(url, userCredentialsPayload);
-			localStorage.setItem('licensedb.token', response.data.token);
+			const { data } = await axios.post(url, userCredentialsPayload);
+			const { access_token, refresh_token, expires_in } = data.data;
 
-			const user = await fetchUserProfile(response.data.token);
+			localStorage.setItem('licensedb.token', access_token);
+			localStorage.setItem('licensedb.refresh_token', refresh_token);
+			let expires_at = Date.now() + expires_in * MILLISEC;
+			localStorage.setItem('licensedb.expires_at', expires_at);
+
+			const user = await fetchUserProfile(access_token);
 			localStorage.setItem(
 				'licensedb.user',
 				JSON.stringify(user.data[0]),
@@ -52,11 +60,10 @@ export function AuthProvider({ children }) {
 	async function OidcSignin() {
 		const { code_verifier, code_challenge } = await generatePKCE();
 		localStorage.setItem('licensedb.codeVerifier', code_verifier);
-		const auth_url = `${process.env.REACT_APP_AUTH_URL}?response_type=code&client_id=${
-			process.env.REACT_APP_CLIENT_ID
-		}&redirect_uri=${encodeURIComponent(
-			process.env.REACT_APP_REDIRECT_URL,
-		)}&scope=openid&code_challenge=${code_challenge}&code_challenge_method=S256&response_mode=fragment`;
+		const auth_url = `${process.env.REACT_APP_AUTH_URL}?response_type=code&client_id=${process.env.REACT_APP_CLIENT_ID
+			}&redirect_uri=${encodeURIComponent(
+				process.env.REACT_APP_REDIRECT_URL,
+			)}&scope=openid&code_challenge=${code_challenge}&code_challenge_method=S256&response_mode=fragment`;
 
 		window.location.href = auth_url;
 	}
@@ -163,32 +170,47 @@ async function getAccessTokenFromRefreshToken() {
 	let expires_at = null;
 	try {
 		const refresh_token = localStorage.getItem('licensedb.refresh_token');
-
-		const response = await axios.post(
-			process.env.REACT_APP_TOKEN_URL,
-			{
-				grant_type: 'refresh_token',
-				client_id: process.env.REACT_APP_CLIENT_ID,
-				refresh_token: refresh_token,
-				scope: 'openid',
-			},
-			{
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
+		if (process.env.REACT_APP_PROVIDER === 'oidc') {
+			const response = await axios.post(
+				process.env.REACT_APP_TOKEN_URL,
+				{
+					grant_type: 'refresh_token',
+					client_id: process.env.REACT_APP_CLIENT_ID,
+					refresh_token: refresh_token,
+					scope: 'openid',
 				},
-			},
-		);
+				{
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded',
+					},
+				},
+			);
 
-		token = response.data.id_token;
-		new_refresh_token = response.data.refresh_token;
-		expires_at = Date.now() + response.data.expires_in * MILLISEC;
+			token = response.data.id_token;
+			new_refresh_token = response.data.refresh_token;
+			expires_at = Date.now() + response.data.expires_in * MILLISEC;
 
-		const user = await fetchUserProfile(token);
+			const user = await fetchUserProfile(token);
 
-		localStorage.setItem('licensedb.token', token);
-		localStorage.setItem('licensedb.expires_at', expires_at);
-		localStorage.setItem('licensedb.refresh_token', new_refresh_token);
-		localStorage.setItem('licensedb.user', JSON.stringify(user.data[0]));
+			localStorage.setItem('licensedb.token', token);
+			localStorage.setItem('licensedb.expires_at', expires_at);
+			localStorage.setItem('licensedb.refresh_token', new_refresh_token);
+			localStorage.setItem('licensedb.user', JSON.stringify(user.data[0]));
+		} else {                                                      // local 
+			const url = `${process.env.REACT_APP_BASE_URL}/refresh-token`;
+			const { data } = await axios.post(url, {
+				refresh_token: refresh_token,
+			});
+			token = data.data.access_token;
+			new_refresh_token = data.data.refresh_token;
+			expires_at = Date.now() + data.data.expires_in * MILLISEC;
+
+			const user = await fetchUserProfile(token);
+
+			localStorage.setItem('licensedb.token', token);
+			localStorage.setItem('licensedb.expires_at', expires_at);
+			localStorage.setItem('licensedb.refresh_token', new_refresh_token);
+			localStorage.setItem('licensedb.user', JSON.stringify(user.data[0]));		}
 	} catch (e) {
 		if (e.response?.data?.status) {
 			if (e.response.data.status !== 409) {
@@ -218,8 +240,7 @@ export async function GetToken() {
 	let token = localStorage.getItem('licensedb.token');
 	const BUFFER_TIME = 60 * MILLISEC * 10; // 10 mins
 	if (
-		Date.now() >= Number(expires_at) - BUFFER_TIME &&
-		process.env.REACT_APP_PROVIDER === 'oidc'
+		Date.now() >= Number(expires_at) - BUFFER_TIME 
 	) {
 		await getAccessTokenFromRefreshToken();
 		token = localStorage.getItem('licensedb.token');
