f1879c2850584e843092cf12cba4725b922c02a1
Dearsh Oberoi <oberoidearsh@gmail.com>
2026-05-20 12:56:43 +0530

    fix(obligations): add delete button in obligations table
    Signed-off-by: Dearsh Oberoi <oberoidearsh@gmail.com>

--- src/pages/obligation.jsx
+++ src/pages/obligation.jsx
@@ -9,18 +9,147 @@ import '../styles/obligation.css';
 import { FcSearch } from 'react-icons/fc';
 import { Link } from 'react-router-dom';
 import { GoPlusCircle } from 'react-icons/go';
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import { useQuery, keepPreviousData, useQueryClient, useMutation } from '@tanstack/react-query';
 import Button from 'react-bootstrap/Button';
-import { fetchObligations } from '../api/api';
+import { fetchObligations, updateObligation } from '../api/api';
 import ObligationDetailForm from './obligationDetailForm';
 import CustomColorCell from '../components/CustomColorCell';
 import '../styles/dataTable.css';
 import '../styles/globalSearch.css';
 import SortableColumnHeader from '../components/SortableColumnHeader';
 import { GetTokenSync } from '../contexts/AuthContext.jsx';
+import { MdOutlineDelete } from "react-icons/md";
+import PropTypes from 'prop-types';
+import { Modal } from 'react-bootstrap';
+import { toast } from 'react-toastify';
 
 const DEFAULT_PER_PAGE = 10;
 const DEFAULT_PAGE = 1;
+
+const TableHeader = () => {
+	return (
+		<div className="table-header my-2">
+			<Link to="/obligation/create">
+				<Button variant="primary">
+					<GoPlusCircle className="me-1 mb-1" />
+					Create Obligation
+				</Button>
+			</Link>
+			<div className="search-container">
+				<div className="search-icon">
+					<FcSearch />
+				</div>
+				<input
+					type="text"
+					placeholder="Search"
+					className="search-input"
+				/>
+			</div>
+		</div>
+	);
+};
+
+function DeletionModal({ obligationToBeDeleted, setObligationToBeDeleted }) {
+	const queryClient = useQueryClient();
+	const mutation = useMutation({
+		mutationFn: updateObligation,
+		onError: error => {
+			toast.error(
+				`Obligation deletion failed: ${error.response.data.error}`,
+				{
+					position: 'top-right',
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: 'dark',
+				},
+			);
+		},
+		onSuccess: data => {
+			toast.success('Obligation deleted successfully!', {
+				position: 'top-right',
+				autoClose: 3000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+				theme: 'dark',
+			});
+			queryClient.invalidateQueries('obligations');
+			queryClient.invalidateQueries('audits');
+		},
+	});
+
+	return (
+		<>
+			<Modal
+				size="lg"
+				show={obligationToBeDeleted !== null}
+				onHide={() => setObligationToBeDeleted(null)}
+			>
+				<Modal.Header
+					style={{ backgroundColor: '#feefef', color: '#c7283e' }}
+					closeButton
+				>
+					<Modal.Title className="fw-bold">
+						Delete Obligation
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body className="fs-5">
+					<div className="fw-bold">
+						This operation hard deletes the obligation. Please use
+						with caution.
+					</div>
+					{obligationToBeDeleted &&
+						obligationToBeDeleted.license_ids.length > 0 && (
+							<div
+								className="alert alert-danger fw-medium mt-3"
+								role="alert"
+							>
+								Obligation cannot be deleted: Found{' '}
+								{obligationToBeDeleted.license_ids.length}{' '}
+								associated licenses. Please disassociate them
+								first.
+							</div>
+						)}
+				</Modal.Body>
+				<Modal.Footer>
+					<button
+						className="btn btn-secondary"
+						onClick={() => setObligationToBeDeleted(null)}
+					>
+						Close
+					</button>
+					<button
+						className="btn btn-danger"
+						onClick={() => {
+							console.log(obligationToBeDeleted)
+							mutation.mutate({
+								obligationPayload: { ...obligationToBeDeleted, active: false },
+								id: obligationToBeDeleted.id,
+							})
+						}}
+						disabled={mutation.isPending || (obligationToBeDeleted?.license_ids.length > 0)}
+					>
+						Delete
+					</button>
+				</Modal.Footer>``
+			</Modal>
+		</>
+	);
+}
+
+DeletionModal.propTypes = {
+	obligationToBeDeleted: PropTypes.shape({
+		topic: PropTypes.string.isRequired,
+		shortnames: PropTypes.arrayOf(PropTypes.string).isRequired,
+	}),
+	setObligationToBeDeleted: PropTypes.func.isRequired,
+};
+
 
 function Obligation() {
 	const [page, setPage] = useState(DEFAULT_PAGE);
@@ -28,6 +157,8 @@ function Obligation() {
 	const [obligationPayload, setObligationPayload] = useState(null);
 	const [sortOrder, setSortOrder] = useState('asc');
 	const isLoggedIn = GetTokenSync() !== null;
+	const [refresh, setRefresh] = useState(false);
+	const [deletionObligation, setDeletionObligation] = useState(null);
 
 	const { isPending, isError, error, data, isPreviousData } = useQuery({
 		queryKey: ['obligations', page, perPage, sortOrder],
@@ -61,7 +192,7 @@ function Obligation() {
 		{
 			name: 'Full Text',
 			wrap: true,
-			maxWidth: '50%',
+			maxWidth: '45%',
 			style: {
 				textAlign: 'left',
 			},
@@ -78,6 +209,27 @@ function Obligation() {
 			maxWidth: '10%',
 			cell: row => <CustomColorCell color={row.classification} />,
 		},
+		{
+			name: 'Actions',
+			maxWidth: '5%',
+			cell: row => (
+				<MdOutlineDelete
+					className="btn-icon"
+					size={20}
+					onClick={() => {
+						if (
+							obligationPayload &&
+							row.id === obligationPayload.id
+						) {
+							setObligationPayload(null);
+							return;
+						}
+						setDeletionObligation(row);
+					}}
+				/>
+			),
+		},
+
 	];
 
 	const handlePageChange = page => {
@@ -98,6 +250,10 @@ function Obligation() {
 
 	return (
 		<div className="content">
+			<DeletionModal
+				obligationToBeDeleted={deletionObligation}
+				setObligationToBeDeleted={setDeletionObligation}
+			/>
 			{isPending ? (
 				<div
 					className="d-flex position-relative"
